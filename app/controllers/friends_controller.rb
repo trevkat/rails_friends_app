27366464db7346495066fdce3d10da902fb911de
Trevor Katilius <111081073+trevkat@users.noreply.github.com>
2024-07-11 15:38:17 -0500

    updated code to allow deleting friends

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,61 +1,54 @@
 class FriendsController < ApplicationController
-  before_action :set_friend, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :set_friend, only: %i[ show edit update destroy ]
 
-  # GET /friends
-  # GET /friends.json
+  # GET /friends or /friends.json
   def index
     @friends = Friend.all
   end
 
-  # GET /friends/1
-  # GET /friends/1.json
+  # GET /friends/1 or /friends/1.json
   def show
   end
 
   # GET /friends/new
   def new
-    #@friend = Friend.new
-    @friend = current_user.friends.build
+    @friend = Friend.new
   end
 
   # GET /friends/1/edit
   def edit
   end
 
-  # POST /friends
-  # POST /friends.json
+  # POST /friends or /friends.json
   def create
-    #@friend = Friend.new(friend_params)
-    @friend = current_user.friends.build(friend_params)
+    @friend = Friend.new(friend_params)
+
     respond_to do |format|
       if @friend.save
-        format.html { redirect_to @friend, notice: 'Friend was successfully created.' }
+        format.html { redirect_to friend_url(@friend), notice: "Friend was successfully created." }
         format.json { render :show, status: :created, location: @friend }
       else
-        format.html { render :new }
+        format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @friend.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /friends/1
-  # PATCH/PUT /friends/1.json
+  # PATCH/PUT /friends/1 or /friends/1.json
   def update
     respond_to do |format|
       if @friend.update(friend_params)
-        format.html { redirect_to @friend, notice: 'Friend was successfully updated.' }
+        format.html { redirect_to friend_url(@friend), notice: "Friend was successfully updated." }
         format.json { render :show, status: :ok, location: @friend }
       else
-        format.html { render :edit }
+        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @friend.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /friends/1
-  # DELETE /friends/1.json
+  # DELETE /friends/1 or /friends/1.json
+
   def destroy
     @friend.destroy
     respond_to do |format|
@@ -70,7 +63,6 @@ class FriendsController < ApplicationController
   end
 
 
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_friend
@@ -79,6 +71,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter, :user_id)
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter)
     end
 end
